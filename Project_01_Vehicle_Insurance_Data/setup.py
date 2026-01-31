949baa5ddda7408c258522d9eaccbe1efd2f5023
Lakshya Hidau <lakshyahidau2005@gmail.com>
2026-01-31 23:51:48 +0530

    Project structure ready, venv created

---
+++ Project_01_Vehicle_Insurance_Data/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Lakshya Hidau",
+    author_email="lakshyahidau2005@gmail.com",
+    package=find_packages()
+)
